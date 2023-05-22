24715762f7b27ec6d29d812e43828a479be97c0c
ki8vi <oneofbln@gmail.com>
2023-05-22 22:50:14 +0600

    This is a new commit for what I originally planned to be amended

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#task-input");
+const taskList = document.querySelector(".content");
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const taskText = input.value;
+
+    const taskHTML = `<div class="task">
+                        <div>${taskText}</div>
+                        <div>
+                            <button>Сделано</button>
+                            <button>Удалить</button>
+                        </div>
+                    </div>`
+
+    taskList.insertAdjacentHTML("beforeend", taskHTML)
+
+    input.value = "";
+    input.focus()
+   
+
+}) 
