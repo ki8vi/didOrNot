bdaf0bf8ed3ecc09cc26648d69988155a372515e
ki8vi <oneofbln@gmail.com>
2023-05-24 23:51:31 +0600

    almost done

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#task-input");
 const taskList = document.querySelector(".content");
+let tasks = [];
 
+if(localStorage.getItem("tasks")) {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+    tasks.forEach(el => {
+    const cssClass = el.done ? "task task-done" : "task"
+    
+    const taskHTML = `<div id="${el.id}"class="${cssClass}">
+                        <div class="task-text">${el.text}</div>
+                        <div class="buttons">
+                            <button data-action="done">Сделано</button>
+                            <button data-action="delete">Удалить</button>
+                        </div>
+                    </div>`
+    
+    taskList.insertAdjacentHTML("beforeend", taskHTML)
+});
 
 form.addEventListener("submit", addTask);
 
@@ -10,8 +27,18 @@ function addTask(e) {
     e.preventDefault();
     
     const taskText = input.value;
-    const taskHTML = `<div class="task">
-                        <div class="task-text">${taskText}</div>
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false
+    };
+    tasks.push(newTask);
+    saveToLocalStrg();
+
+    const cssClass = newTask.done ? "task-done" : "task"
+    
+    const taskHTML = `<div id="${newTask.id}"class="${cssClass}">
+                        <div class="task-text">${newTask.text}</div>
                         <div class="buttons">
                             <button data-action="done">Сделано</button>
                             <button data-action="delete">Удалить</button>
@@ -32,15 +59,25 @@ taskList.addEventListener("click", doneTask);
 
 
 function deleteTask(event){
-    if(event.target.dataset.action === "delete") {
-        const parent = event.target.closest(".task");
-        parent.remove()
-    }
+    if(event.target.dataset.action !== "delete") return;
+    const parent = event.target.closest(".task");
+    const id = +parent.id;
+    const index = tasks.findIndex(t => t.id === id) 
+    tasks.splice(index, 1)
+    parent.remove()
+    saveToLocalStrg();
 }
 function doneTask(event) {
-    if(event.target.dataset.action === "done") {
-        const parent = event.target.closest(".task")
-        parent.classList.toggle("task-done")
-    }
+    if(event.target.dataset.action !== "done") return;
+    const parent = event.target.closest(".task");
+    const id = +parent.id;
+    const index = tasks.findIndex(t => t.id === id);
+    tasks[index].done = !tasks[index].done;
+    parent.classList.toggle("task-done");
+    saveToLocalStrg();
+}
+
+function saveToLocalStrg() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
